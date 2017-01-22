3bcebce4269d85b4ba0bf2699fdead787fb8af90
Timm Friebe <thekid@thekid.de>
2017-01-22 20:19:19 +0100

    Add constructor accepting mailboxes as array or as ews.MailboxDataArray

--- src/main/php/ews/messages/GetUserAvailabilityRequest.class.php
+++ src/main/php/ews/messages/GetUserAvailabilityRequest.class.php
@@ -1,6 +1,23 @@
 <?php namespace ews\messages;
 
+use ews\types\FreeBusyViewOptions;
+
 /** @see https://msdn.microsoft.com/en-us/library/office/aa563800(v=exchg.150).aspx */
 class GetUserAvailabilityRequest extends \ews\Object {
-  
+
+  /**
+   * Creates a new "UserAvailability" request
+   *
+   * @param  ews.MailboxData[]|ews.MailboxDataArray $mailboxes
+   * @param  ews.FreeBusyViewOptions $freeBusyViewOptions
+   */
+  public function __construct($mailboxes= null, $freeBusyViewOptions= null) {
+    parent::__construct(nameof($this));
+    if ($mailboxes instanceof MailboxDataArray) {
+      $this->with($mailboxes);
+    } else if (is_array($mailboxes)) {
+      $this->with(new MailboxDataArray($mailboxes));
+    }
+    $freeBusyViewOptions && $this->with($freeBusyViewOptions);
+  }
 }
